575ba237af245d7e5d06c81f08dc510705346a92
João Eliandro <eliandrogermano@gmail.com>
2020-09-24 18:34:27 -0300

    Fixed hook

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,23 +30,67 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const productsFromStorage = await AsyncStorage.getItem(
+        '@GoBarber:cart:products',
+      );
+
+      if (productsFromStorage) {
+        setProducts(JSON.parse(productsFromStorage));
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const setProductsToAsyncStorage = useCallback(() => {
+    AsyncStorage.setItem('@GoBarber:cart:products', JSON.stringify(products));
+  }, [products]);
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const addToCart = useCallback(
+    async product => {
+      const productIndex = products.findIndex(el => el.id === product.id);
+      if (productIndex > -1) {
+        const updatedProducts = [...products];
+        updatedProducts[productIndex].quantity += 1;
+        setProducts(updatedProducts);
+      } else {
+        setProducts([...products, { ...product, quantity: 1 }]);
+      }
+      setProductsToAsyncStorage();
+    },
+    [products, setProductsToAsyncStorage],
+  );
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const increment = useCallback(
+    async id => {
+      const productIndex = products.findIndex(el => el.id === id);
+      const product = products[productIndex];
+      product.quantity += 1;
+      const updatedProducts = [...products];
+      updatedProducts[productIndex] = product;
+      setProducts(updatedProducts);
+      setProductsToAsyncStorage();
+    },
+    [products, setProductsToAsyncStorage],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      const productIndex = products.findIndex(el => el.id === id);
+      const product = products[productIndex];
+      const updatedProducts = [...products];
+
+      if (product.quantity === 1) {
+        updatedProducts.splice(productIndex, 1);
+      } else {
+        product.quantity -= 1;
+        updatedProducts[productIndex] = product;
+      }
+      setProducts(updatedProducts);
+      setProductsToAsyncStorage();
+    },
+    [products, setProductsToAsyncStorage],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
